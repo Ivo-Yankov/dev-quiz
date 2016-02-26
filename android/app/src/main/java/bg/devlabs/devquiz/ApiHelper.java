5f94b16024e86ea9781f6ab80f86c2c4c8b4af58
Simona Stoyanova <sstoyanova26@gmail.com>
2016-02-26 20:51:52 +0200

    added android app fo real

---
+++ android/app/src/main/java/bg/devlabs/devquiz/ApiHelper.java
@@ -0,0 +1,16 @@
+package bg.devlabs.devquiz;
+
+/**
+ * Created by Simona Stoyanova on 2/26/2016.
+ * Dev Labs
+ * simona@devlabs.bg
+ */
+public class ApiHelper {
+
+    public static final String STATUS_OK = "OK";
+
+    public static String aAnswer = "a";
+    public static String bAnswer = "b";
+    public static String cAnswer = "c";
+    public static String dAnswer = "d";
+}
